4a4dbead078953d595c2eabda427846020068b4c
SKatiyar <katiyar.suyash@gmail.com>
2015-01-10 20:28:16 +0530

    better formating in default

--- formatter.go
+++ formatter.go
@@ -18,6 +18,22 @@ func (d *defaultFormatter) Format(f Fields) ([]byte, error) {
 	if val, ok := f["level"]; ok {
 		str += colorArr[val.(level)]
 	}
+
+	if tUid, ok := f["uid"]; ok {
+		str += fmt.Sprintf("[ %s ] %+v ", "uid", tUid)
+		delete(f, "uid")
+	}
+
+	if tLvl, ok := f["level"]; ok {
+		str += fmt.Sprintf("[ %s ] %+v ", "level", tLvl)
+		delete(f, "level")
+	}
+
+	if tVal, ok := f["time"]; ok {
+		str += fmt.Sprintf("[ %s ] %+v ", "time", tVal)
+		delete(f, "time")
+	}
+
 	for key, value := range f {
 		str += fmt.Sprintf("[ %s ] %+v ", key, value)
 	}
